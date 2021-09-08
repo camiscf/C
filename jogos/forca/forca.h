a223ffe3a1fd6fe99fe9b2f28608b403ee368b78
Camila <87675978+camiscf@users.noreply.github.com>
2021-09-08 14:25:56 -0300

    Create forca.h

---
+++ jogos/forca/forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+int acertou();
+int enforcou();
+int chuteserrados();
+int jachutou(char letra);
